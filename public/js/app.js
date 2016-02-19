8060d6fccbef190869750733f5beb0f6993a8fae
Sachin Rathod <sachin.rathod284@gmail.com>
2016-02-19 16:42:35 +0530

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,13 @@ var socket = io();
 
 socket.on('connect',function(){
 console.log('Connected to socket.io server!');
+
+socket.emit('message',{
+	text : 'Welcome to the chat application of client !'
+});
+});
+
+socket.on('message',function(message){
+	console.log('New message');
+	console.log(message.text);
 });
