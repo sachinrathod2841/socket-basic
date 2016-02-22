6e7991b0808c79c6e2299cecb0d5ffa44849b75a
Sachin Rathod <sachin.rathod284@gmail.com>
2016-02-22 16:21:14 +0530

    Add support for chat rooms

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,13 @@ var name = getQueryVariable('name') || 'Anonymous';
 var room = getQueryVariable('room');
 var socket = io();
 console.log(name +' wants to join '+ room );
+jQuery('.room-title').text(room);
 socket.on('connect',function(){
 	console.log('Connected to socket.io server!');
+	socket.emit('joinRoom',{
+		name : name,
+		room : room
+	});
 });
 
 
